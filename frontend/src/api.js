d47646ec0efdd2e26e9b7a32f20c32909157e9e1
hamseungjun <hamseungjun1029@gmail.com>
2025-07-30 10:54:54 +0900

    env_commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    // baseURL: `/api`, // 환경 변수에서 API URL을 가져옵니다.
+    // baseURL: 'http://localhost:8000/api',
+    baseURL: `/api`, // 환경 변수에서 API URL을 가져옵니다.
     withCredentials: true, // 쿠키를 주고받기 위한 필수 설정
 });
 
